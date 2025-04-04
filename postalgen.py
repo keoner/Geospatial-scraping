858459e937be5e45610bb43ff808f1a1827d8caf
Keoni <136683521+keoner@users.noreply.github.com>
2025-04-05 02:24:26 +0800

    Update postalgen.py

--- postalgen.py
+++ postalgen.py
@@ -1,10 +1,12 @@
 import random
 import time
+import re
 import googlemaps
 
 found = False
 api_key = 'YOUR_API_KEY'
 gmaps = googlemaps.Client(key=api_key)
+i = 1
 
 def postalgen():
     districtcode = random.randint(1, 82)
@@ -28,25 +30,27 @@ def reverse_geocoding(location):
     reversed_result = gmaps.reverse_geocode((location.get("lat"), location.get("lng")))
     return reversed_result
 
-def main(reversed_result, postalcode, location):
-    while found != True:
+def main(reversed_result, postalcode, location, i=1):
+    word = r"\bSingapore\b"
+    while True:
         if reversed_result:
             for place in reversed_result:
                 formatted_address = place.get("formatted_address")
                 if formatted_address != '9R29+RW Singapore':
-                    print("\n")
-                    print(f"Coordinates for {postalcode}: {location}")
-                    print(place.get("formatted_address"))
-                    print("https://www.google.com/maps/place/?q=place_id:"+ reversed_result[0].get("place_id"))
-                    found == True
-                    time.sleep(2)
-                new_postalcode = postalgen()
-                new_location = geocoding(new_postalcode)
-                new_reversed_result = reverse_geocoding(new_location)
-                main(new_reversed_result, new_postalcode, new_location)
-                time.sleep(2)
-                        
+                    formatted_address = place.get("formatted_address")
+                    index = re.search(word, formatted_address).end()
+                    if postalcode == formatted_address[index+1:]:
+                        print("\n")
+                        print(f"Coordinates for {postalcode}: {location}")
+                        print(formatted_address)
+                        print("https://www.google.com/maps/place/?q=place_id:"+ reversed_result[0].get("place_id"))
+                    time.sleep(0.3)
+                    new_postalcode = postalgen()
+                    new_location = geocoding(new_postalcode)
+                    new_reversed_result = reverse_geocoding(new_location)
+                    main(new_reversed_result, new_postalcode, new_location, i)
+
 postalcode = postalgen()
 location = geocoding(postalcode)
-reversed_result = reverse_geocoding(location)
-main(reversed_result, postalcode, location)
+reverse_geocoding_result = reverse_geocoding(location)
+main(reverse_geocoding_result, postalcode, location, i)
